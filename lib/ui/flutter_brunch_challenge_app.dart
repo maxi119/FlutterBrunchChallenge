57378361fffbf76515b2db4ae134ca1044bfc75c
Rebecca Hsieh <rebecca.hsieh77@gmail.com>
2020-08-28 13:38:30 +0800

    [refactor] 調整檔案結構

---
+++ lib/ui/flutter_brunch_challenge_app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'home/home_screen.dart';
+
+class FlutterBrunchChallengeApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Brunch Challenge',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
---
+++ lib/ui/home/home_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        child: Center(child: Text('// TODO')),
+      ),
+    );
+  }
+}
