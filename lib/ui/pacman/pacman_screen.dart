ded2411bc13c43fd9ec713d2a2fe04cf315616f9
Rebecca Hsieh <rebecca.hsieh77@gmail.com>
2020-09-18 19:37:26 +0800

    [feature] 新增 地圖基底表格

--- lib/ui/pacman/pacman_screen.dart
+++ lib/ui/pacman/pacman_screen.dart
@@ -6,10 +6,13 @@ class PacManScreen extends StatefulWidget {
 }
 
 class _PacManScreenState extends State<PacManScreen> {
+  final numberInRow = 10;
+  final numberOfSquares = 10 * 10;
+
   @override
   Widget build(BuildContext context) {
-///    小精靈圖檔
-///    Image.asset("images/pacman.png")
+    ///    小精靈圖檔
+    ///    Image.asset("images/pacman.png")
     return Scaffold(
       backgroundColor: Colors.black,
       body: Column(
@@ -17,14 +20,25 @@ class _PacManScreenState extends State<PacManScreen> {
           Expanded(
             flex: 5,
             child: Container(
-              color: Colors.red.shade200,
-///              child:   TODO 加上地圖
-            ),
+                color: Colors.red.shade200,
+                child: GridView.builder(
+                  itemCount: numberOfSquares,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: numberInRow,
+                  ),
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                      color: Colors.grey,
+                      margin: EdgeInsets.all(1),
+                    );
+                  },
+                )),
           ),
           Expanded(
             child: Container(
               color: Colors.red,
-///              child:   這邊可以放 分數 或 開始遊戲 按鈕
+
+              ///              child:   這邊可以放 分數 或 開始遊戲 按鈕
             ),
           ),
         ],
